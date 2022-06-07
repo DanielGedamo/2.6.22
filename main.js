2c438ffb7c98d879f5481f6502537eb4354af4cd
DANIELGEDAMO <danielgadamo@gmail.com>
2022-06-07 09:37:05 +0300

    1

---
+++ main.js
@@ -0,0 +1,29 @@
+function fuonForAge() {
+  var userAge =
+    new Date().getFullYear() -
+    document.getElementById("userBirth").value.slice(0, 4);
+  if (userAge > 18) {
+    document.getElementById("welcome").innerText =
+      "welcome" 
+    document.getElementById("welcome").style.color = "blue";
+  } else {
+    document.getElementById("no").innerHTML = "no enter"+  "<img src=https://c.tenor.com/-0wLd8WRKLQAAAAM/cl03.gif>";
+    document.getElementById("no").style.color = "red";
+  }
+}
+function getgreen() {
+  document.getElementById("no").style.color = "green";
+  document.getElementById("welcome").style.color = "green";
+}
+var myArray = []
+function follNameList() {
+  var firstName =document.getElementsByClassName("first name")
+  var lestName =document.getElementById("lest name")
+  if ( myArray.length <4) {
+      document.getElementsByClassName.
+      
+  }
+  for(var i =0; i< firstName[i] ; i++){
+      
+  }
+}
